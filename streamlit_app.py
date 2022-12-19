aa594060c42200534e0b8644708b26f9fa5b37e0
VandanaK3 <121009845+VandanaK3@users.noreply.github.com>
2022-12-19 14:26:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,10 @@
 
 import streamlit
-streamlit.title('my parents new healthy dinner')
+streamlit.title('breakfast fevorite')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('Avacado toast')
 
                
                
