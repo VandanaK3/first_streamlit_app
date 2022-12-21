79921beda80edfc3b279a9015725a57ea9c07e99
VandanaK3 <121009845+VandanaK3@users.noreply.github.com>
2022-12-21 17:37:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -62,8 +62,7 @@ def get_fruit_load_list():
         my_cur.execute("SELECT * from fruit_load_list")
         return my_cur.fetchall()
     
-    
-    if streamlit.button('get_fruit_load_list'):
+ if streamlit.button('get_fruit_load_list'):
       my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
       my_data_rows=get_fruit_load_list()
       streamlit.dataframe(my_data_rows)
